ab508c2779692bfbf85e65bda64fc67e4bfc0c12
MacBook <aliahmedalihassan@gmail.com>
2019-11-25 00:40:02 +0300

    changes in Server and Client Code

--- Messages.enum.js
+++ Messages.enum.js
@@ -6,7 +6,7 @@ Messages Enums
 
 
 module.exports ={
-     control_1 : 'Sending stable weight value 100gm',
+     control_1 : 'stable weight value 100gm',
      control_2 : 'Command is not Executable',
      control_3 : 'Balance overload range',
      control_4 : 'Balance underload range'
--- promp.js
+++ promp.js
@@ -1,6 +1,6 @@
 var stdin = process.openStdin();
 
-stdin.addListener("data", function(d) {
+stdin.addListener(console.log("ss"),function(d) {
     // note:  d is an object, and when converted to a string it will
     // end with a linefeed.  so we (rather crudely) account for that  
     // with toString() and then trim() 
